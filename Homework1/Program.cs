3eac178a74c6591746ad18069919e94ca0a7e079
Vsevolod <vsevolod.lashin@gmail.com>
2022-07-16 18:59:29 +0300

    New homework

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -1,4 +1,20 @@
 ﻿/* Task 1: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+int Big (int n1, int n2)
+{
+    int biggest;
+    int smallest;
+    if (n1 < n2)
+    {
+        biggest = n2;
+        smallest = n1;
+    }  
+    else
+    {
+        biggest = n1;
+        smallest = n2;
+    }
+    return biggest;
+}
 
 Console.Write("Input first number: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
@@ -6,18 +22,20 @@ int num1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input second number: ");
 int num2 = Convert.ToInt32(Console.ReadLine());
 
-if (num1 < num2)
-{
-    Console.WriteLine("Min number: " + num1);
-    Console.WriteLine("Max number: " + num2);
-}  
-else
-{
-    Console.WriteLine("Min number: " + num2);
-    Console.WriteLine("Max number: " + num1);
-}
+int result = Big (num1, num2);
+
+Console.Write($"Наибольшее число: {result}");
 
 // Task 2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+int biggest(int n1, int n2, int n3)
+{
+    int result = n1;
+    if(n1 > n2 && n1 > n3) result = n1;
+    if(n2 > n1 && n2 > n3) result = n2;
+    if(n3 > n1 && n3 > n2) result = n3;
+    return result;
+}
 
 Console.Write("Input first number: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
@@ -27,36 +45,44 @@ int num2 = Convert.ToInt32(Console.ReadLine());
 
 Console.Write("Input third number: ");
 int num3 = Convert.ToInt32(Console.ReadLine());
-int max;
-if (num1 > num2)
-    max = num1;
-else
-    max = num2;
-if (num3 > max)
-    max = num3;
-Console.WriteLine("Max number: " + max);
+
+int res = biggest(num1, num2, num3);
+Console.Write($"Наибольшее число: {res}");
 
 // Task 3: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка)
 
+string chet(int n1)
+{
+    string result = "Да";
+    if((n1 % 2) == 0) result = "Да";
+    if((n1 % 2) != 0) result = "Нет";
+    return result;
+}
 Console.Write("Input number: ");
 int num = Convert.ToInt32(Console.ReadLine());
 
-if ((num % 2) == 0)
-    Console.WriteLine("Четное");
-else
-    Console.WriteLine("Нечетное");
+string res = chet(num);
+Console.Write($"Результат: {res}");
 
 // Task 4: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
-
+void allchet(int n)
+{
+    int current = 1;
+    int result = 1;
+    while (current < n)
+    {
+        if ((current % 2) == 0)
+        {
+            result = current;;
+            Console.Write($"{result} ");
+        }
+        current++;
+    }
+}
 Console.Write("Input number: ");
 int num = Convert.ToInt32(Console.ReadLine());
-int current = 1;
 
-while (current <= num)
-{
-    if (current % 2 == 0)
-        Console.Write(current + " ");
-    current ++;
-}
+allchet(num);
 */
+
 

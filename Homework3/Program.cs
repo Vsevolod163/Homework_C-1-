2eac5aa70ddbc9c637d4b2691e33f783d6f7bb70
Vsevolod <vsevolod.lashin@gmail.com>
2022-07-17 01:12:11 +0300

    New file

---
+++ Homework3/Program.cs
@@ -0,0 +1,18 @@
+﻿//Task1 Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом
+void Polindrom(int n)
+{
+    while(n > 100000 || n < -100000 || n < 10000 && n > -10000) 
+    {   
+        Console.Write("Ошибка. Введите число еще раз: ");
+        n = Convert.ToInt32(Console.ReadLine());
+    }    
+        int z = n / 10000;
+        int k = (n / 1000) % 10;
+        if(z == (n % 10) && k == ((n / 10) % 10)) Console.Write("Является ли число палиндромом? Да");
+          
+        else Console.Write("Является ли число палиндромом? Нет");          
+}
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Polindrom(num);
